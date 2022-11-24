eae7fa3ea1dee3a8956a0099e63a7c1fbe425068
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-11-23 21:05:03 -0300

    change workspace

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -4,5 +4,5 @@ export const CardWrap = styled("div", {
     background: "#282A3A",
     aspectRatio: "9/16",
     borderRadius: "15px",
-    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.2)",
+    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.3)",
 });
