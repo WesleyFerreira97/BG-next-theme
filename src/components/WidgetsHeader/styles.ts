97733d568f9dc634dc3d22baa5dfa8cb29939296
Wesley Ferreira <wesleyferreira97@outlook.com>
2023-01-04 12:38:45 -0300

    style: component widget header

--- src/components/WidgetsHeader/styles.ts
+++ src/components/WidgetsHeader/styles.ts
@@ -1,20 +1,33 @@
 import { styled } from "stitches.config";
 
 export const TitleWrap = styled("h3", {
+    margin: ".25rem auto 1.25rem",
+    color: "$secondary",
+    themeFont: "title",
     themeFontSize: {
-        // default: "$11",
-        lg: "$1",
-        md: "$10",
-        sm: "$9",
-        xs: "$8",
+        xs: "$5",
+        sm: "$8",
     },
-    margin: "1rem auto",
 });
 
 export const SubTitleWrap = styled("span", {
+    themeFont: "text",
+    color: "$secondary",
+    fontWeight: "500",
+    themeFontSize: {
+        xs: "$3",
+        sm: "$4",
+    }
 });
 
 export const ParagraphWrap = styled("p", {
+    themeFont: "text",
+    color: "$secondary",
+    fontWeight: "300",
+    themeFontSize: {
+        xs: "14px",
+        sm: "$3",
+    }
 });
 
 export const WidgetHeaderWrap = styled("div", {
